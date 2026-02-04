dcba55df956acba25b60c4bfe6eaa77ee86b7500
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-02-04 09:00:42 +0530

    feat: add embedded business and bank details for seller

--- src/main/java/com/ankitsaahariya/entities/BusinessDetails.java
+++ src/main/java/com/ankitsaahariya/entities/BusinessDetails.java
@@ -1,4 +1,15 @@
 package com.ankitsaahariya.entities;
 
+import lombok.Data;
+
+@Data
 public class BusinessDetails {
+
+    private String businessName;
+    private String businessEmail;
+    private String businessMobil;
+    private String businessAddress;
+
+    private String logo;
+    private String banner;
 }
