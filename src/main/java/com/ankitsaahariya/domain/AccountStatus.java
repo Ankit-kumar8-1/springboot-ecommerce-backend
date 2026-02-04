56a145f7683ed0b90d3f8a292df9768c3c217c03
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-02-04 08:50:25 +0530

    feat: add seller and review entities with account status management
    - Added Review entity with product and user associations
    - Implemented Seller entity with business, bank, and pickup address details
    - Introduced AccountStatus enum to manage seller account lifecycle
    - Added role-based seller setup with email verification and default status

---
+++ src/main/java/com/ankitsaahariya/domain/AccountStatus.java
@@ -0,0 +1,11 @@
+package com.ankitsaahariya.domain;
+
+public enum AccountStatus {
+    PENDING_VERIFICATION, // Account create ho chuka hai, par email/phone verification abhi pending hai
+    ACTIVE,               // Account active hai aur user system use kar sakta hai
+    SUSPENDED,            // Account temporarily band hai due to rule violation ya admin action
+    DEACTIVATED,          // Account user ya system ne deactivate kar diya hai, ab use nahi ho sakta
+    BANNED,               // Account permanently ban ho chuka hai due to serious violations
+    CLOSED                // Account permanently close ho gaya hai aur dobara activate nahi ho sakta
+
+}
--- src/main/java/com/ankitsaahariya/entities/Review.java
+++ src/main/java/com/ankitsaahariya/entities/Review.java
@@ -1,4 +1,46 @@
 package com.ankitsaahariya.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String reviewText;
+
+    @Column(nullable = false)
+    private String rating;
+
+    @ElementCollection
+    private List<String> productImage;
+
+    @JsonIgnore
+    @ManyToOne
+    @Column(nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @Column(nullable = false)
+    private UserEntity user;
+
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
 }
